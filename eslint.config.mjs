18f8f584aa1a754dc5e58890c0be8c7b572e08ed
Pulatov Yunus <pulat2007@mail.ru>
2026-04-15 02:10:05 +0500

    second commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,30 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from 'eslint-config-prettier/flat';  
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  ...tseslint.configs.recommended,  
+  prettier,
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@/semi": "off",
+      "@typescript-eslint/semi": "warn",
+      "@typescript-eslint/no-empty-interface": [
+        "error",
+        { allowSingleExtends: true }
+      ],
+      
+    },
+  },
 ]);
 
 export default eslintConfig;
